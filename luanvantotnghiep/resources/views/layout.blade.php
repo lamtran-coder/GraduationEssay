1feb58c69e398cbeb1c04ffbebb2081c83f9682b
Lamtran <lam.tt.982201@gmail.com>
2021-06-08 16:18:06 +0700

    co file nen .zip

--- luanvantotnghiep/resources/views/layout.blade.php
+++ luanvantotnghiep/resources/views/layout.blade.php
@@ -74,17 +74,18 @@ License URL: http://creativecommons.org/licenses/by/3.0/
         </div>
         <div class="cssmenu">
            <ul>
-             <li class="active"><a href="register.html">Sign up & Save</a></li> 
-             <li><a href="shop.html">Store Locator</a></li> 
-             <li><a href="login.html">My Account</a></li> 
-             <li><a href="checkout.html">CheckOut</a></li> 
+             <li class="active"><a href="register.html">Đăng ký & Lưu</a></li> 
+             <li><a href="shop.html">Định vị</a></li> 
+             <li><a href="login.html">Tài khoản</a></li> 
+             <li><a href="checkout.html">Thanh toán</a></li> 
            </ul>
         </div>
         <ul class="icon2 sub-icon2 profile_img">
             <li><a class="active-icon c2" href="#"> </a>
                 <ul class="sub-icon2 list">
-                    <li><h3>Products</h3><a href=""></a></li>
-                    <li><p>Lorem ipsum dolor sit amet, consectetuer  <a href="">adipiscing elit, sed diam</a></p></li>
+                    <li><h3>Gỏ hàng</h3><a href=""></a></li>
+                    <li><h3>sản phẩm</h3></li>
+                    <li><p><a href="">vào gỏ</a></p></li>
                 </ul>
             </li>
         </ul>
